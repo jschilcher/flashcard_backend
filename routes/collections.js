522ea500f7044d2b50450f8f09942382f84406db
Jenelle Schilcher <jschilcher8@gmail.com>
2021-07-05 13:57:36 -0500

    Add router.delte

--- routes/collections.js
+++ routes/collections.js
@@ -15,7 +15,7 @@ router.get("/", async (req, res) => {
 router.get("/:id", async (req, res) => {
     try {
         const cardPackage = await CardPackage.findById(req.params.id);
-        
+
         if(!cardPackage)
             return res.status(400).send(`The card package with id "${req.params.id}" does not exist`);
 
@@ -44,5 +44,71 @@ router.post("/", async (req, res) => {
         return res.status(500).send(`Internal Server Error: ${ex}`);
     }
 })
+
+router.get("/:cardPackageId/cards", async (req, res) => {
+    try {
+        const cardPackage = await CardPackage.findById(req.params.cardPackageId);
+        if (!cardPackage) 
+            return res.status(400).send(`The cardpackage with id "${req.params.cardPackageId}" does not exist.`);
+
+        return res.send(cardPackage);
+
+    }   catch (ex) {
+        return res.status(500).send(`Internal Server Error: ${ex}`);
+    }
+});
+
+router.get("/:cardPackageId/cards/:cardId", async (req, res) => {
+    try {
+        const cardPackage = await CardPackage.findById(req.params.cardPackageId);
+        if (!cardPackage) 
+            return res.status(400).send(`The cardpackage with id "${req.params.cardPackageId}" does not exist.`);
+           
+        const card = await Card.findById(req.params.cardId);
+        if (!card)
+            return res.status(400).send(`The card with id "${req.params.cardId}`);
+        
+        return res.send(card);
+
+    } catch (ex) {
+        return res.status(500).send(`Internal Server error: ${ex}`);
+    }
+});
+
+router.post("/:cardPackageId/cards/:cardId", async (req, res) => {
+    try {
+        const cardPackage = await CardPackage.findById(req.params.cardPackageId);
+        if(!cardPackage) return res.status(400).send(`The product with id "${req.params.cardPackageId}" does not exist.`);
+
+        const card = await Card.findById(req.params.cardId);
+        if (!card) return res.status(400).send(`The card with id "${req.params.cardId}" does not exist.`);
+
+        cardPackage.cards.push(card);
+
+        await cardPackage.save();
+        return res.send(cardPackage.cards);
+    } catch (ex) {
+        return res.status(500).send(`Internal server error: ${ex}`);
+    }
+});
+
+
+router.delete("/:cardPackageId/cards/:cardId", async (req, res) => {
+    try {
+        const cardPackage = await CardPackage.findById(req.params.cardPackageId);
+        if(!cardPackage) return res.status(400).send(`The product with id "${req.params.cardPackageId}" does not exist.`);
+
+        let card = cardPackage.cards.id(req.params.cardId);
+        if (!card) return res.status(400).send(`The card with id "${req.params.cardId}" does not exist.`);
+
+        card = await card.remove();
+
+        await cardPackage.save();
+        return res.send(card);
+
+    } catch (ex) {
+        return res.status(500).send(`Internal server error ${ex}`);
+    }   
+}); 
 
 module.exports = router;
